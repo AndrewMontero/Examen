493b29d01d343b02062ffe69bfc0b19d8ee4614e
Andrew <andrewmontero0805@gmail.com>
2023-10-04 10:40:49 -0600

    Examen

---
+++ Examen/src/main/java/MAIN/examen/Examen.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package MAIN.examen;
+
+import GUI.Calculadora;
+
+
+/**
+ *
+ * @author Andrew
+ */
+public class Examen {
+
+    public static void main(String[] args) {
+        Calculadora exam = new Calculadora();
+        exam.setVisible(true);
+    }
+}
